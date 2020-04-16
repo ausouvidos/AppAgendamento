6fe9cd25e74e9be1fe36db87ad356989fa09aa87
Rodrigo Romano <rodrigo@rodrigoromano.net>
2020-04-16 09:58:11 +0200

    fix duplicated availabilities

--- Models/AvailabilityDates.cs
+++ Models/AvailabilityDates.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Models
 {
     public class AvailabilityDates
     {
+        public override bool Equals(object obj)
+        {
+            var item = obj as AvailabilityDates;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return item.Start == Start && item.End == End;
+        }
+
+        public override int GetHashCode() =>
+            Start.GetHashCode() & End.GetHashCode();
+
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
     }
