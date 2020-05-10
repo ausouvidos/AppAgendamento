22b91074073016bc66809508d0ed694e06509361
Lucas Rodrigues <lucas.rodrigues@mtac.org.br>
2020-05-10 12:39:16 -0300

    add cursor pointer to admin table

--- Web/ClientApp/src/views/empresas-pendente-aprovacao.vue
+++ Web/ClientApp/src/views/empresas-pendente-aprovacao.vue
@@ -3,7 +3,7 @@
     <h1>Empresas pendentes aprovação</h1>
 
     <div>
-      <el-table :data="companies" style="width: 100%" @row-click="openItem">
+      <el-table :data="companies" class="table-admin table-clickable" @row-click="openItem">
         <TableColumn prop="name" label="Nome da Instituição"></TableColumn>
         <TableColumn prop="city" label="Cidade"></TableColumn>
         <TableColumn prop="state" label="UF"></TableColumn>
@@ -20,99 +20,97 @@
       <template v-slot:modal-title>Aprovar empresa</template>
       <form>
           <div class="form-group">
-            <label for="company-name">Nome da instituição<small aria-hidden="true">*</small></label>
-              <input
-                type="text"
-                id="company-name"
-                v-model="selectedCompany.name"
-                :class="['form-control']"
-                :disabled="true">
+            <label for="company-name">Nome da instituição</label>
+            <input
+              type="text"
+              id="company-name"
+              v-model="selectedCompany.name"
+              :class="['form-control']"
+              :disabled="true">
           </div>
           <div class="form-group">
-            <label for="company-address">Endereço<small aria-hidden="true">*</small></label>
+            <label for="company-address">Endereço</label>
+            <input
+              type="text"
+              id="company-address"
+              v-model="selectedCompany.address"
+              :class="['form-control']"
+              :disabled="true">
+          </div>
+          <div class="form-row">
+            <div class="form-group col-sm">
+              <label for="company-district">Bairro</label>
               <input
                 type="text"
-                id="company-address"
-                v-model="selectedCompany.address"
+                id="company-district"
+                v-model="selectedCompany.district"
                 :class="['form-control']"
                 :disabled="true">
-          </div>
-          <div class="form-row">
-            <div class="form-group col-sm">
-              <label for="company-district">Bairro<small aria-hidden="true">*</small></label>
-                <input
-                  type="text"
-                  id="company-district"
-                  v-model="selectedCompany.district"
-                  :class="['form-control']"
-                  :disabled="true">
             </div>
             <div class="form-group col-sm">
-              <label for="company-city">Cidade<small aria-hidden="true">*</small></label>
-                <input
-                  type="text"
-                  id="company-city"
-                  v-model="selectedCompany.city"
-                  :class="['form-control']"
-                  :disabled="true">
+              <label for="company-city">Cidade</label>
+              <input
+                type="text"
+                id="company-city"
+                v-model="selectedCompany.city"
+                :class="['form-control']"
+                :disabled="true">
             </div>
           </div>
           <div class="form-row">
             <div class="form-group col-sm">
-              <label for="company-state">Estado<small aria-hidden="true">*</small></label>
-                <select
-                  id="company-state"
-                  v-model="selectedCompany.state"
-                  :class="['custom-select']"
-                  :disabled="true">
-                  <option value="">Selecione</option>
-                  <option v-for="(estado, index) in estados" :key="index">{{ estado }}</option>
+              <label for="company-state">Estado</label>
+              <select
+                id="company-state"
+                v-model="selectedCompany.state"
+                :class="['custom-select']"
+                :disabled="true">
+                <option value="">Selecione</option>
+                <option v-for="(estado, index) in estados" :key="index">{{ estado }}</option>
                 </select>
             </div>
             <div class="form-group col-sm">
-              <label for="company-zipCode">CEP<small aria-hidden="true">*</small></label>
-                <the-mask
-                  type="text"
-                  id="company-zipCode"
-                  placeholder="99999-999"
-                  mask="#####-###"
-                  v-model="selectedCompany.zipCode"
-                  :class="['form-control']"
-                  :disabled="true" />
-
+              <label for="company-zipCode">CEP</label>
+              <the-mask
+                type="text"
+                id="company-zipCode"
+                placeholder="99999-999"
+                mask="#####-###"
+                v-model="selectedCompany.zipCode"
+                :class="['form-control']"
+                :disabled="true" />
             </div>
           </div>
           <div class="form-row">
             <div class="form-group col-sm">
-              <label for="company-phone">Telefone<small aria-hidden="true">*</small></label>
-                <the-mask
-                  type="tel"
-                  id="company-phone"
-                  placeholder="(99) 99999-9999"
-                  v-model="selectedCompany.phone"
-                  :class="['form-control']"
-                  :mask="['(##) ####-####', '(##) #####-####']"
-                  :disabled="true" />
+              <label for="company-phone">Telefone</label>
+              <the-mask
+                type="tel"
+                id="company-phone"
+                placeholder="(99) 99999-9999"
+                v-model="selectedCompany.phone"
+                :class="['form-control']"
+                :mask="['(##) ####-####', '(##) #####-####']"
+                :disabled="true" />
             </div>
             <div class="form-group col-sm">
-              <label for="company-contactPerson">Nome para contato<small aria-hidden="true">*</small></label>
-                <input
-                  type="text"
-                  id="company-contactPerson"
-                  v-model="selectedCompany.contactPerson"
-                  :class="['form-control']"
-                  :disabled="true">
+              <label for="company-contactPerson">Nome para contato</label>
+              <input
+                type="text"
+                id="company-contactPerson"
+                v-model="selectedCompany.contactPerson"
+                :class="['form-control']"
+                :disabled="true">
             </div>
           </div>
           <div class="form-group">
-            <label for="quantidade-vouchers">Quantidade de Vouchers<small aria-hidden="true">*</small></label>
-              <input
-                type="number"
-                id="quantidade-vouchers"
-                v-model="quantidade"
-                :class="['form-control']">
+            <label for="quantidade-vouchers">Quantidade de Vouchers</label>
+            <input
+              type="number"
+              id="quantidade-vouchers"
+              v-model="quantidade"
+              :class="['form-control']">
           </div>
-        
         </form>
     </b-modal>
   </div>
