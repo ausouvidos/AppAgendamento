c46c67ed4b22961d6943e3ed0ad56c0ceeef7596
Vitor A. Vale <vitoravale@outlook.com>
2022-05-22 22:07:19 -0300

    feat: restore 72h lock

--- Services/Availability/GetWeeklyAvailabilitieMinStartDateCommandHandler.cs
+++ Services/Availability/GetWeeklyAvailabilitieMinStartDateCommandHandler.cs
@@ -22,12 +22,11 @@ namespace Services.Availability
         {
             var lastAppointment = _db.Availabilities.Where(a => a.CustomerEmail == request.Email).OrderByDescending(a => a.Start).FirstOrDefault();
             DateTime minStart = DateTime.UtcNow.AddHours(1);
-            return Task.FromResult(minStart);
-            //if (lastAppointment == null || lastAppointment.Start.AddHours(72) < minStart)
-            //{
-            //    return Task.FromResult(minStart);
-            //}
-            //return Task.FromResult(lastAppointment.Start.AddHours(72));
+            if (lastAppointment == null || lastAppointment.Start.AddHours(72) < minStart)
+            {
+                return Task.FromResult(minStart);
+            }
+            return Task.FromResult(lastAppointment.Start.AddHours(72));
         }
     }
 }
