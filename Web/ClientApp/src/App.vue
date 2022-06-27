63c1bb2df16b9b85efe40e54dbf8b97766b11599
Djonatas Tenfen <djonatas@live.com>
2022-06-27 17:48:17 -0300

    feat/ new background

--- Web/ClientApp/src/App.vue
+++ Web/ClientApp/src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app" class="wrapper">
+    <div
+      :style="{
+        'background-image': `url(${require('./assets/background.jpg')})`,
+        width: '100%',
+        height: '100%',
+        'z-index': 999999,
+        'background-repeat': 'no-repeat',
+        'background-position': 'center',
+        'background-size': 'cover',
+        'min-height': '100vh',
+      }"
+    >
       <header class="header">
         <div class="header-inner container-xl bg-transparent">
           <a class="logo" href="https://www.ausouvidos.com/">
@@ -8,6 +20,7 @@
                       ./assets/logo-aus-ouvidos@2x.png 2x,
                       ./assets/logo-aus-ouvidos@3x.png 3x"
               src="./assets/logo-aus-ouvidos.png"
+              style="max-width: 200px;max-height: 200px;margin-top: 15px;margin-bottom: 15px;"
               alt="Projeto Aus Ouvidos">
           </a>
         </div>
@@ -16,20 +29,28 @@
         <router-view />
       </section>
       <footer class="footer">
-        <div class="container-xl">
-          &copy; Copyright Aus Ouvidos, 2020. Todos os direitos reservados. | Realização
-          <a href="https://www.mtac.org.br/" target="_blank">
-            MTAC
-            <img
-              srcset="./assets/logo-mtac.png,
-                      ./assets/logo-mtac@2x.png 2x,
-                      ./assets/logo-mtac@3x.png 3x"
-              src="./assets/logo-mtac.png"
-              alt="MTAC">
-          </a>
+        <div class="container-footer">
+          <img src="./assets/ausouvidos-footer.png" />
+          <ul class="footer-social-ul">
+            <li style="display: flex;align-items: center;">
+              <img class="footer-social-img" src="./assets/linkedin-logo.png" />
+            </li>
+            <li style="display: flex;align-items: center;">
+              <img class="footer-social-img" src="./assets/facebook-logo.png" />
+            </li>
+            <li style="display: flex;align-items: center;">
+              <img class="footer-social-img" src="./assets/instagram-logo.png" />
+            </li>
+          </ul>
+          <div style="display: flex;align-items: center;">
+            <p style="font-size:16px; line-height:1.3em;">
+              <span style="letter-spacing:0em;"><span style="font-size:16px;">2022 - Instituto Aus Ouvidos. Direitos Reservados.</span></span>
+            </p>
+          </div>
         </div>
       </footer>
     </div>
+  </div>
 </template>
 
 <style src="./styles/main.scss" lang="scss"></style>
